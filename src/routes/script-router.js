2c0bf33bade26745e73f6827693961d49390bc27
Deliman206 <smiller206@me.com>
2018-05-14 13:48:46 -0700

    added script router

--- src/routes/script-router.js
+++ src/routes/script-router.js
@@ -1,21 +1,23 @@
 'use strict';
 
-import { json } from 'body-parser';
+import { Json } from 'body-parser';
 import { Router } from 'express';
 import HttpError from 'http-errors';
 import Script from '../model/script-model';
-import logger from '../lib/logger';
 
 const scriptRouter = new Router();
-const jsonParser = new json();
+const jsonParser = new Json();
 
 scriptRouter.post('/api/script', jsonParser, (request, response, next) => {
-  if(!request.body.title) return next(new HttpError(400, 'Bad Content: Title Required'));
+  if (!request.body.title) return next(new HttpError(400, 'Bad Content: Title Required'));
   return new Script(request.body).save()
     .then(script => response.json(script))
     .catch(next);
 });
 
-scriptRouter.get('/api/script/:id', (request, response, next) => {
-
+scriptRouter.get('/api/script/:title', (request, response, next) => {
+  if (!request.params.id) return next(new HttpError(400, 'Bad Content: id is required'));
+  return Script.findOne(request.params.title)
+    .then(script => response.json(script))
+    .catch(next);
 });
