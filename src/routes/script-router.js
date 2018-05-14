be7bb40c24049bdcc01581cdde360956d4b0108f
Deliman206 <smiller206@me.com>
2018-05-14 15:27:09 -0700

    added keyword model, added script scrub to post route

--- src/routes/script-router.js
+++ src/routes/script-router.js
@@ -8,16 +8,29 @@ import Script from '../model/script-model';
 const scriptRouter = new Router();
 const jsonParser = new Json();
 
-scriptRouter.post('/api/script', jsonParser, (request, response, next) => {
-  if (!request.body.title) return next(new HttpError(400, 'Bad Content: Title Required'));
+scriptRouter.post('/script', jsonParser, (request, response, next) => {
+  if (!request.body) return next(new HttpError(400, 'Bad Content: Title Required'));
   return new Script(request.body).save()
-    .then(script => response.json(script))
+    .then((script) => {
+      // scrub logic
+      const keywords = script.match() 
+      // returns array
+      const solution = [];
+      for (let i = 0; i < keywords.length; i++) {
+        solution.push(new keywords(keywords[i], i));
+      }
+      // for loop over aqrray to touch each string
+      // assign Keyword parameters of content and position
+     
+      // return array of keywords
+    })
+    .then(keywords => response.json(keywords))
     .catch(next);
 });
 
-scriptRouter.get('/api/script/:title', (request, response, next) => {
-  if (!request.params.id) return next(new HttpError(400, 'Bad Content: id is required'));
-  return Script.findOne(request.params.title)
-    .then(script => response.json(script))
-    .catch(next);
-});
+// scriptRouter.get('/script/:title', (request, response, next) => {
+//   if (!request.params.id) return next(new HttpError(400, 'Bad Content: id is required'));
+//   return Script.findOne(request.params.title)
+//     .then(script => response.json(script))
+//     .catch(next);
+// });
